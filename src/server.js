5464a19b7758402de3231cbfd671e0e73905d282
Aman Dwivedi <19dwivediaman@gmail.com>
2021-05-23 00:04:18 +0530

    server setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const PORT = 4000;
+const app = express();
+
+const handleListening = () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+
+
+app.listen(PORT, handleListening );
