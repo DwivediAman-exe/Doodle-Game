7caf46d66ff3585b7c8c661dc394cb9867ce6ac8
Aman Dwivedi <19dwivediaman@gmail.com>
2021-05-23 00:15:14 +0530

    pug

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+import { join } from 'path';
 import express from 'express';
 
 const PORT = 4000;
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', join(__dirname, 'views'));
+
+app.get('/', (req,res) => {
+	res.render('home');
+})
 
 const handleListening = () =>
   console.log(`✅ Server running on http://localhost:${PORT}`);
